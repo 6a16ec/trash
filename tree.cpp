badfebd4aa515d6ae56e9991f8c005b8645b63e6
6a16ec <semaev2014@yandex.ru>
2018-01-30 20:09:45 +0300

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -5,7 +5,22 @@ struct element
 {
     int count_branches = 0;
     element* branches[5000];
+    int max_len = -1;
 };
+
+void findMaxLen(element* element)
+{
+    int max_len = 0;
+
+    for(int i = 0; i < (*element).count_branches; i++)
+    {
+        if((*(*element).branches[i]).max_len == -1) findMaxLen((*element).branches[i]);
+
+        if((*(*element).branches[i]).max_len + 1 > max_len) max_len = (*(*element).branches[i]).max_len + 1;
+    }
+
+    (*element).max_len = max_len;
+}
 
 
 int main()
@@ -24,10 +39,32 @@ int main()
         (*elements[parent-1]).count_branches += 1;
     }
 
+    findMaxLen(elements[0]);
+
+    int max_way = 0;
+
     for(int i = 0; i < n; i++)
     {
-        cout << i << " " << (*elements[i]).count_branches << endl;
+        if((*elements[i]).max_len * 2 + 1 > max_way)
+        {
+            int max1 = 0, max2 = 0;
+
+            for(int j = 0; j < (*elements[i]).count_branches; j++)
+            {
+                int max_now = (*(*elements[i]).branches[j]).max_len + 1;
+                if(max_now > max1 || max_now > max2)
+                {
+                    max1 = max(max1, max2);
+                    max2 = max_now;
+                }
+            }
+
+            int way_now = max1 + max2 + 1;
+            if(way_now > max_way) max_way = way_now;
+        }
     }
+
+    cout << max_way;
 
     return 0;
 }
